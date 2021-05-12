6b036799ea97340efd86fa8049f5734503f8171b
GIKIK7 <giikik@wp.pl>
2021-05-12 18:31:17 +0200

    Zadanie2
    usuniecie printa zeby nie wyswietlalo none

--- Lab4/Zadanie2/client.py
+++ Lab4/Zadanie2/client.py
@@ -3,4 +3,4 @@ import sys
 hello_client = Client('http://localhost:8000/?wsdl')
 print("podaj liczbe decymalnie")
 l1 = input()
-print hello_client.service.say_hello(l1)
+hello_client.service.say_hello(l1)
